684f3e49e3babdd037c4809f21dedfb96cd5c324
rhsupplies <communications@rhsupplies.org>
2026-09-15 09:47:01 +0200

    Add files via upload

---
+++ precache-manifest.f295d34f95974a787943524d6b47b6fd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78ff29784973999f68c7ebc4e2de8e9",
+    "url": "/cga/index.html"
+  },
+  {
+    "revision": "67e6f02d5081f0e19130",
+    "url": "/cga/static/css/main.a633a874.chunk.css"
+  },
+  {
+    "revision": "5c2b70374b5d5b69a82b",
+    "url": "/cga/static/js/2.e072c60d.chunk.js"
+  },
+  {
+    "revision": "67e6f02d5081f0e19130",
+    "url": "/cga/static/js/main.01ea1cb8.chunk.js"
+  },
+  {
+    "revision": "c53c2baa90612ee5470d",
+    "url": "/cga/static/js/runtime-main.b81ab860.js"
+  },
+  {
+    "revision": "4cb0d52042403f7285cccc34f9a927a9",
+    "url": "/cga/static/media/calculator.4cb0d520.svg"
+  },
+  {
+    "revision": "e055be8db563ef939b0ef25e63bb6c63",
+    "url": "/cga/static/media/cga_logo_actual_size.e055be8d.svg"
+  },
+  {
+    "revision": "cbbeb4de19cfcb931884002a666d5d18",
+    "url": "/cga/static/media/compass.cbbeb4de.svg"
+  },
+  {
+    "revision": "7b872744029d134d494b4b07c02f6126",
+    "url": "/cga/static/media/downloadPrintReport.7b872744.svg"
+  },
+  {
+    "revision": "94ff1c4bb67bc22fbde320ea4c3a9967",
+    "url": "/cga/static/media/facebook.94ff1c4b.svg"
+  },
+  {
+    "revision": "5ebef0e44cdc3e37aabbfceb3d532dc7",
+    "url": "/cga/static/media/graph.5ebef0e4.svg"
+  },
+  {
+    "revision": "d4e021554e4951fe3a64cb5b8a514990",
+    "url": "/cga/static/media/loading.d4e02155.svg"
+  },
+  {
+    "revision": "5e304232bee0eeed19cfc286db378cbd",
+    "url": "/cga/static/media/method_frequency_infographic.5e304232.svg"
+  },
+  {
+    "revision": "f15ec190d83e14abd76c115f20edcd41",
+    "url": "/cga/static/media/mouse-pointer.f15ec190.svg"
+  },
+  {
+    "revision": "20f2411a4b5b7fa98d42a43f356d2fa5",
+    "url": "/cga/static/media/rhsc_cga_multi_img_background.20f2411a.png"
+  },
+  {
+    "revision": "33837545cca8cad99c242564b7ab1207",
+    "url": "/cga/static/media/rhsc_logo_actual_size.33837545.svg"
+  },
+  {
+    "revision": "1938b42974740873334c62a5ac15c874",
+    "url": "/cga/static/media/sector_infographic.1938b429.png"
+  },
+  {
+    "revision": "b4a98fcb724bafa9c045083a084321e3",
+    "url": "/cga/static/media/spending_infographic.b4a98fcb.svg"
+  },
+  {
+    "revision": "83692b23a81de78239aba846ff95af9c",
+    "url": "/cga/static/media/twitter.83692b23.svg"
+  },
+  {
+    "revision": "dd6327405e218c118d7ba23bd8ba7ca4",
+    "url": "/cga/static/media/youtube.dd632740.svg"
+  }
+]);
